8ea071f315063eec5aad49efae79c0ca1ac535d5
katharina-spiecker <ilkanoadmin@Ilkas-MBP.fritz.box>
2020-09-11 15:17:57 +0200

    Updates

---
+++ precache-manifest.f8f39815667e39ccd024dbd1a36c88f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0b4b92f58ec8baf2155da67c1deb3e",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "f5aa80ae567d84c4114d",
+    "url": "/react-calculator/static/css/main.09528025.chunk.css"
+  },
+  {
+    "revision": "dc17dd3d20bc57a23c44",
+    "url": "/react-calculator/static/js/2.c8bb0d18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.c8bb0d18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5aa80ae567d84c4114d",
+    "url": "/react-calculator/static/js/main.9edfbb1a.chunk.js"
+  },
+  {
+    "revision": "14453d16db6c0e44d6da",
+    "url": "/react-calculator/static/js/runtime-main.7b47f775.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
